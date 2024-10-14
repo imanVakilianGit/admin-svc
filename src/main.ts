00017dc752cee1e87332cb4d96bfc7a5e4073df2
imanvakilian <imanvakiliangit@gmail.com>
2024-10-14 10:37:16 +0330

    main : (feat) configure app to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { RpcException, TcpOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import { AppModule } from './app.module';
+import { APP_NAME } from './common/constant/service-name.constant';
+
+async function bootstrap(): Promise<void> {
+    const port: number = Number(process.env.APP_PORT);
+
+    const app = await NestFactory.createMicroservice<TcpOptions>(AppModule, {
+        transport: Transport.TCP,
+        options: {
+            port,
+        },
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            stopAtFirstError: true,
+            forbidNonWhitelisted: true,
+            forbidUnknownValues: true,
+            disableErrorMessages: true,
+            exceptionFactory: (errors) => {
+                console.log(new Date().toLocaleString('fa'), ' ------ validation error: ', errors);
+                return new RpcException({
+                    service: APP_NAME,
+                    success: false,
+                    statusCode: 400,
+                    message: errors[0]?.constraints
+                        ? Object.values(errors[0].constraints)[0]
+                        : Object.values(errors[0].children?.[0]?.constraints || {})[0],
+                    code: 'VALIDATION_ERROR',
+                    field: errors[0]?.constraints ? errors[0].property : errors[0].children?.[0]?.property,
+                });
+            },
+        }),
+    );
+
+    await app.listen();
+    console.log('👽 ~ admin-svc:', `server running on port: ${port}`);
 }
 bootstrap();
